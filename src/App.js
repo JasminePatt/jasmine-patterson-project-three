ab39edfb4639488f0d6a5587a39942c6b8195f2c
Jasmine <jacamoe@live.ca>
2021-10-03 01:13:07 -0400

    Connected database and displayed data on page

--- src/App.js
+++ src/App.js
@@ -5,26 +5,65 @@ import './App.css';
 
 function App() {
 
+
+// USE STATE VARIABLES
+
 // Stateful value and function, 'notes' as the local variable and 'setNotes' as the function to update the state
 const [notes, setNotes] = useState([]);
 
+
+// USE EFFECT
+
 // useEffect to run when the component first mounts
 useEffect( () => {
-// Creating database referennce
+  // Creating database referennce
   const dbRef = ref(realtimeData);
 
   onValue(dbRef, (snapshot) => {
-    console.log(snapshot.val())
+    // console.log(snapshot.val())
     const myData = snapshot.val();
+
+  // Empty array for the notes object
+  const newArray = [];
+  // 'For in' loop to gain access to all objects in the array
+
+  for (let propName in myData) {
+    console.log(propName);
+
+    // Save the loop in a new variable
+    const notesObject = {
+      key: propName,
+      note: myData[propName]
+    }
+
+    // Push all new input on to the notesObject onto the Array
+    newArray.push(notesObject)
+  }
+// Passing our state the array
+  setNotes(newArray);
   });
   
 }, []);
 
 
 
+
+
   return (
     <div>
       <h1>Planted Ideas</h1>
+
+      {/* Container for page content(notes) */}
+      <ul> {
+        notes.map( (eachNote) => {
+          return (
+            <li key={eachNote.key}>
+                <p>{eachNote.note}</p>
+            </li>
+          )
+        })
+        }
+      </ul>
     </div>
   )
 }
